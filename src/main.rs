70ef2c9ccb87c3e339869efeaaadefda3d184e81
Raphael Queiroz <raphael@chips.gg>
2023-11-29 10:33:24 -0300

    adding function to mensure execution time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod merkle_tree;
+mod merkle_metrics;
 
 fn main() {
+    // merkle_metrics::run_metrics();
     merkle_tree::run_demo();
 }
 
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,11 +1,10 @@
 use sha2::{Digest, Sha256};
-
-struct MerkleTree {
-    nodes: Vec<Vec<u8>>, // Armazena os hashes de todos os nós
+pub struct MerkleTree {
+    pub nodes: Vec<Vec<u8>>, // Armazena os hashes de todos os nós
 }
 
 impl MerkleTree {
-    fn new(data: Vec<u8>) -> Self {
+    pub fn new(data: Vec<u8>) -> Self {
         let mut tree = MerkleTree { nodes: Vec::new() };
         tree.create_tree(data);
         tree
@@ -16,7 +15,7 @@ impl MerkleTree {
         let total_nodes = 2 * leaf_count - 1;
         let mut nodes = vec![vec![0; 32]; total_nodes]; // Inicializa todos os nós com hashes vazios
 
-        println!("max capacity {}", 2 * leaf_count - 1);
+        // println!("max capacity {}", 2 * leaf_count - 1);
 
         // Preenche os nós folha
         for (i, &datum) in data.iter().enumerate() {
@@ -29,12 +28,12 @@ impl MerkleTree {
         for i in (0..leaf_count - 2).rev() {
             let left_hash = nodes[i * 2 + 1].clone();
             let right_hash = nodes[i * 2 + 2].clone();
-            println!(
-                "index i {} hash left {} right {}",
-                i,
-                hex::encode(&left_hash),
-                hex::encode(&right_hash)
-            );
+            // println!(
+            //     "index i {} hash left {} right {}",
+            //     i,
+            //     hex::encode(&left_hash),
+            //     hex::encode(&right_hash)
+            // );
 
             let mut hasher = Sha256::new();
             hasher.update(&left_hash);
@@ -42,19 +41,19 @@ impl MerkleTree {
             nodes[i] = hasher.finalize().to_vec();
         }
 
-        for node in &nodes {
-            println!("conferindo arvore {}", hex::encode(node));
-        }
+        // for node in &nodes {
+        //     println!("conferindo arvore {}", hex::encode(node));
+        // }
 
         self.nodes = nodes;
     }
 
-    fn generate_proof(&self, data_index: u8) -> Vec<(Vec<u8>, bool)> {
+    pub fn generate_proof(&self, data_index: u8) -> Vec<(Vec<u8>, bool)> {
         let mut proof = Vec::new();
         let leaf_count = (self.nodes.len() / 2).next_power_of_two();
 
         let mut node_index = leaf_count + data_index as usize - 1; // Índice do nó folha no vetor 'nodes'
-        println!("node index {}", node_index);
+        // println!("node index {}", node_index);
 
         // getting brother
         let sibling_index = if node_index % 2 == 0 {
@@ -62,7 +61,7 @@ impl MerkleTree {
         } else {
             node_index + 1
         };
-        println!("brother index {} node index {}", sibling_index, node_index);
+        // println!("brother index {} node index {}", sibling_index, node_index);
         if sibling_index < self.nodes.len() {
             proof.push((self.nodes[sibling_index].clone(), node_index % 2 == 0));
         }
@@ -75,11 +74,11 @@ impl MerkleTree {
                 } else {
                     parent_index + 1
                 };
-                println!(
-                    "missing hash {} hash: {}",
-                    missing_hash,
-                    hex::encode(&self.nodes[missing_hash])
-                );
+                // println!(
+                //     "missing hash {} hash: {}",
+                //     missing_hash,
+                //     hex::encode(&self.nodes[missing_hash])
+                // );
                 proof.push((self.nodes[missing_hash].clone(), parent_index % 2 == 0));
             }
             node_index = parent_index;
@@ -91,7 +90,7 @@ impl MerkleTree {
         proof
     }
 
-    fn verify_proof(
+    pub fn verify_proof(
         &self,
         proof: Vec<(Vec<u8>, bool)>,
         data: u8,
@@ -102,7 +101,7 @@ impl MerkleTree {
         let mut current_hash = hasher.finalize().to_vec();
 
         for (hash, position) in proof {
-            println!("proff elemenet {}", hex::encode(&hash));
+            // println!("proff elemenet {}", hex::encode(&hash));
             let mut hasher = Sha256::new();
             if position {
                 hasher.update(&hash);
@@ -115,11 +114,11 @@ impl MerkleTree {
             current_hash = hasher.finalize().to_vec();
         }
 
-        println!(
-            "root hash: {} current hash {}",
-            hex::encode(&root_hash),
-            hex::encode(&current_hash)
-        );
+        // println!(
+        //     "root hash: {} current hash {}",
+        //     hex::encode(&root_hash),
+        //     hex::encode(&current_hash)
+        // );
         current_hash == root_hash
     }
 }
@@ -132,7 +131,7 @@ pub fn run_demo() {
     let data_value = 2; // O valor do dado em si
 
     // // Gera a prova para o dado
-    let proof = merkle_tree.generate_proof(data_index);
+    let proof: Vec<(Vec<u8>, bool)> = merkle_tree.generate_proof(data_index);
 
     // // Obtém o hash da raiz da árvore
     let root_hash = merkle_tree.nodes[0].clone();
